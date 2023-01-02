64c29d7d910bec3fed04571afbca781ed38434fc
Malik <dbmalek92@gmail.com>
2023-01-02 18:48:42 +0100

    style :  New arrivals

--- src/pages/home/HomePages.jsx
+++ src/pages/home/HomePages.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Annocument from '../../components/annocument/Annocument'
+import Discount from '../../components/discount/Discount'
+import FlashDeals from '../../components/flashDeals/FlashDeals'
 import Main from '../../components/main/Main'
+import NewArrivals from '../../components/newarrivals/NewArrivals'
+import TopCate from '../../components/top/TopCate'
 
 const HomePages = () => {
   return (
     <div>
       <Main />
+      <FlashDeals />
+      <TopCate />
+      <NewArrivals />
+      <Discount />
+      <Annocument />
     </div>
   )
 }
