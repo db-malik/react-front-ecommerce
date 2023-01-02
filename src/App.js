ca037581d13a865c0de854605cc37ebf078cf3fa
Malik <dbmalek92@gmail.com>
2023-01-02 18:57:26 +0100

    style: cart

--- src/App.js
+++ src/App.js
@@ -4,16 +4,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 import Header from './common/header/Header'
 import Footer from './common/footer/Footer'
-import Data from './data/Data'
-import shopData from './components/shops/Sdata'
 import HomePages from './pages/home/HomePages'
 
 function App() {
-  const { productItems } = Data
-  const { shopItems } = shopData
-
-  const [CartItem, setCartItem] = useState([])
-
   return (
     <>
       <Router>
@@ -21,8 +14,6 @@ function App() {
         <Router>
           <Switch>
             <Route exact path="/" component={HomePages} />
-            {/* <Route exact path="/Product" component={<div>product</div>} />
-            <Route exact path="/cart" component={<div>cart</div>} /> */}
           </Switch>
         </Router>
 
